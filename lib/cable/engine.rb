7ef33a5280b7ed673ac021a59a18dbd7aa4c4259
Bredon Jones <bredon.jones@gmail.com>
2014-12-12 16:17:17 -0600

    puts assets in expected place

--- lib/cable/engine.rb
+++ lib/cable/engine.rb
@@ -16,12 +16,6 @@ require 'open-uri'
 
 module Cable
   class Engine < Rails::Engine
-
-    
-    initializer "static assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-      app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
-    end
     
     config.autoload_paths << "#{root}/app/sweepers"
     config.active_record.observers = :cable_sweeper
